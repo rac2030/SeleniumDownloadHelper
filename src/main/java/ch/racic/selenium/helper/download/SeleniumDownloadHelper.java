4384ea66d3fdb71035253b0d1cbf934642936a5b
rac2030 <m@rac.su>
2014-06-08 16:53:57 +0200

    Added base64 js lib and minimize operation for packaging

---
+++ src/main/java/ch/racic/selenium/helper/download/SeleniumDownloadHelper.java
@@ -0,0 +1,12 @@
+/*
+ * Copyleft (c) 2014. This code is for learning purposes only. Do whatever you like with it but don't take it as perfect code.
+ * Michel Racic (http://rac.su/+) => github.com/rac2030
+ */
+
+package ch.racic.selenium.helper.download;
+
+/**
+ * Created by rac on 08.06.14.
+ */
+public class SeleniumDownloadHelper {
+}
